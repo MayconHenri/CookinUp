6e6951a6ec2e31d54897bd0377bea3542be99726
Maycon <mayconoficial14@hotmail.com>
2024-06-23 12:27:59 -0300

    Projeto finalizado!

--- src/components/CardCategoria.vue
+++ src/components/CardCategoria.vue
@@ -1,30 +1,33 @@
 <script lang="ts">
-import type ICategoria from "@/interfaces/iCategoria"
-import type { PropType } from "vue"
-import Tag from "./Tag.vue"
+import type ICategoria from '@/interfaces/ICategoria'
+import type { PropType } from 'vue';
+import Tag from './Tag.vue';
+import IngredienteSelecionavel from './IngredienteSelecionavel.vue';
 
 export default {
   props: {
-    categoria: { type: Object as PropType<ICategoria>, required: true },
+    categoria: { type: Object as PropType<ICategoria>, required: true }
   },
-  components: { Tag },
+  components: { Tag, IngredienteSelecionavel },
+  emits: ['adicionarIngrediente', 'removerIngrediente']
 }
 </script>
 
 <template>
   <article class="categoria">
     <header class="categoria__cabecalho">
-      <img
-        :src="`/imagens/icones/categorias_ingredientes/${categoria.imagem}`"
-        alt="categoria__imagem"
-      />
-      <p class="paragrafo-lg categoria__nome" :categoria>
-        {{ categoria.nome }}
-      </p>
+      <img :src="`/imagens/icones/categorias_ingredientes/${categoria.imagem}`" alt="" class="categoria__imagem">
+
+      <h2 class="paragrafo-lg categoria__nome">{{ categoria.nome }}</h2>
     </header>
+
     <ul class="categoria__ingredientes">
       <li v-for="ingrediente in categoria.ingredientes" :key="ingrediente">
-        <Tag :texto="ingrediente" />
+        <IngredienteSelecionavel
+          :ingrediente="ingrediente"
+          @adicionar-ingrediente="$emit('adicionarIngrediente', $event)"
+          @remover-ingrediente="$emit('removerIngrediente', $event)"
+        />
       </li>
     </ul>
   </article>
@@ -35,7 +38,7 @@ export default {
   width: 19.5rem;
   padding: 1rem;
   border-radius: 1rem;
-  background: var(--branco, #fff);
+  background: var(--branco, #FFF);
   box-shadow: 4px 4px 10px 0px rgba(68, 68, 68, 0.05);
   height: 100%;
 
@@ -58,7 +61,7 @@ export default {
 
 .categoria__nome {
   text-align: center;
-  color: var(--verde-medio, #3d6d4a);
+  color: var(--verde-medio, #3D6D4A);
   font-weight: 700;
 }
 
--- src/components/SelecionarIngredientes.vue
+++ src/components/SelecionarIngredientes.vue
@@ -1,18 +1,21 @@
 <script lang="ts">
-import { obterCategorias } from "@/http/index"
-import type ICategoria from "@/interfaces/iCategoria"
-import CardCategoria from "./CardCategoria.vue"
+import { obterCategorias } from '@/http/index';
+import type ICategoria from "@/interfaces/ICategoria"
+import CardCategoria from './CardCategoria.vue';
+import BotaoPrincipal from './BotaoPrincipal.vue';
 
 export default {
+  name: 'SelecionarIngredientes',
   data() {
     return {
-      categorias: [] as ICategoria[],
-    }
+      categorias: [] as ICategoria[]
+    };
   },
   async created() {
-    this.categorias = await obterCategorias()
+    this.categorias = await obterCategorias();
   },
-  components: { CardCategoria }
+  components: { CardCategoria, BotaoPrincipal },
+  emits: ['adicionarIngrediente', 'removerIngrediente', 'buscarReceitas']
 }
 </script>
 
@@ -26,13 +29,19 @@ export default {
 
     <ul class="categorias">
       <li v-for="categoria in categorias" :key="categoria.nome">
-        <CardCategoria :categoria="categoria" />
+        <CardCategoria
+          :categoria="categoria"
+          @adicionar-ingrediente="$emit('adicionarIngrediente', $event)"
+          @remover-ingrediente="$emit('removerIngrediente', $event)"
+        />
       </li>
     </ul>
 
     <p class="paragrafo dica">
       *Atenção: consideramos que você tem em casa sal, pimenta e água.
     </p>
+
+    <BotaoPrincipal texto="Buscar receitas!" v-on:click="$emit('buscarReceitas')"/>
   </section>
 </template>
 
@@ -44,7 +53,7 @@ export default {
 }
 
 .titulo-ingredientes {
-  color: var(--verde-medio, #3d6d4a);
+  color: var(--verde-medio, #3D6D4A);
   display: block;
   margin-bottom: 1.5rem;
 }
